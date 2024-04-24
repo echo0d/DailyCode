c73fddf19a9219f60ff8c3f5c3beffb601ead496
echo0d <echo0d@163.com>
2024-04-24 23:01:55 +0800

    4.24backup

--- GoLearn/ch4/rev/rev.go
+++ GoLearn/ch4/rev/rev.go
@@ -9,8 +9,15 @@ func reverse(s []int) {
 	}
 }
 func main() {
+	// 反转
 	a := [...]int{0, 1, 2, 3, 4, 5}
 	reverse(a[:])
 	fmt.Println(a) // "[5 4 3 2 1 0]"
-
+	// 将slice元素循环向左旋转n个元素
+	s := []int{0, 1, 2, 3, 4, 5}
+	// Rotate s left by two positions.
+	reverse(s[:2])
+	reverse(s[2:])
+	reverse(s)
+	fmt.Println(s) // "[2 3 4 5 0 1]"
 }
