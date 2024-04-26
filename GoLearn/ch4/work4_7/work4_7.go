06adee37c6f62d817b0baaffcf7cce3772c940c5
echo0d <echo0d@163.com>
2024-04-26 18:05:35 +0800

    4.26backup

--- GoLearn/ch4/work4_7/work4_7.go
+++ GoLearn/ch4/work4_7/work4_7.go
@@ -1,11 +1,11 @@
+//修改reverse函数用于原地反转UTF-8编码的[]byte。是否可以不用分配额外的内存？
+
 package main
 
 import (
 	"fmt"
 )
 
-// run command
-// go run ./src/chapter4/work4_7/work4_7.go
 func main() {
 	s := []byte("Hello 世界")
 
@@ -13,7 +13,9 @@ func main() {
 }
 
 func reverse(bs []byte) []byte {
+	// 先把byte数组转成rune数组
 	runes := []rune(string(bs))
+	// 然后正常些，和原本的reverse函数一样
 	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
 		runes[i], runes[j] = runes[j], runes[i]
 	}
