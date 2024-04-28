03d8f340e7d22e53d0504d1da938268f0aa1599a
echo0d <echo0d@163.com>
2024-04-28 18:03:22 +0800

    04428

---
+++ GoLearn/ch4/embed/embed.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+type Point struct {
+	X, Y int
+}
+
+type Circle struct {
+	Point
+	Radius int
+}
+
+type Wheel struct {
+	Circle
+	Spokes int
+}
+
+func main() {
+	var w Wheel
+	// NOTE: 结构体字面值方法一
+	w = Wheel{Circle{Point{8, 8}, 5}, 20}
+	// NOTE: 结构体字面值方法二
+	// NOTE: 花括号的结尾要加逗号
+	w = Wheel{
+		Circle: Circle{
+			Point:  Point{X: 8, Y: 8},
+			Radius: 5,
+		},
+		Spokes: 20,
+	}
+
+	fmt.Printf("%#v\n", w)
+	// Output:
+	// Wheel{Circle:Circle{Point:Point{X:8, Y:8}, Radius:5}, Spokes:20}
+
+	w.X = 42
+
+	fmt.Printf("%#v\n", w)
+	// Output:
+	// Wheel{Circle:Circle{Point:Point{X:42, Y:8}, Radius:5}, Spokes:20}
+
+}
