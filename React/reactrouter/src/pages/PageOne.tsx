88f3721c8f912a5adef2504963fecd862afd0000
echo0d <echo0d@163.com>
2024-06-17 20:48:11 +0800

    react router

---
+++ React/reactrouter/src/pages/PageOne.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PageOne() {
+    return (
+        <React.Fragment>
+            <h2 style={{textAlign: 'center'}}>页面组件一</h2>
+        </React.Fragment>
+    );
+}
+
+export default PageOne;
