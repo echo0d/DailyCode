c04bfd2b34539bd1e25a954e57a53b302e59b48c
echo0d <echo0d@163.com>
2024-12-09 18:05:26 +0800

    C#执行exe/dll

---
+++ PEExecute/CsharpExecutor/CsharpExecutor/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace CsharpExecutor
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            // 1. 执行本地exe文件，使用Process.start()
+            Local.ExeExecutor();
+            // 2. 执行本地exe文件(非反射)
+            Local.DllExecutor();
+
+        }
+        
+        
+        
+    }
+}
---
+++ PEExecute/loadCalc/loadCalc/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace loadCalc
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class aaa
+    {
+        public static void bbb()
+        {
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = "c:\\windows\\system32\\calc.exe";
+            p.Start();
+        }
+    }
+}
