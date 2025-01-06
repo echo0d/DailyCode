50c75318454dcdb2644b6987554817cc2d1ea327
echo0d <echo0d@163.com>
2025-01-06 20:04:00 +0800

    vite-vue3

---
+++ VUE/vue-3-vite/src/HelloVueApp.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue';
+
+const message = ref('Hello Vue!!');
+
+function sayHello() {
+  alert(message.value);
+}
+</script>
+
+<template>
+  <div>{{ message }}</div>
+  <button @click="sayHello">Say Hello</button>
+</template>
---
+++ VUE/vue-3-vite/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import HelloVueApp from './HelloVueApp.vue'
+import HelloWorldApp from './components/HelloWorld.vue'
+
+
+createApp(HelloVueApp).mount('#hello-vue');
+createApp(HelloWorldApp).mount('#hello-world');
+
+
+
